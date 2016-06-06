665e27b7b1c69b8754a5b6ec8bd43a51006c6410
zsm-tang <tjwabc@gmail.com>
2016-06-06 10:33:28 +0800

    只能发送 图片和照相

--- easeui/src/main/java/com/easemob/easeui/ui/EaseChatFragment.java
+++ easeui/src/main/java/com/easemob/easeui/ui/EaseChatFragment.java
@@ -104,10 +104,15 @@ public class EaseChatFragment extends EaseBaseFragment implements EMEventListene
     static final int ITEM_PICTURE = 2;
     static final int ITEM_LOCATION = 3;
     
-    protected int[] itemStrings = { R.string.attach_take_pic, R.string.attach_picture, R.string.attach_location };
-    protected int[] itemdrawables = { R.drawable.ease_chat_takepic_selector, R.drawable.ease_chat_image_selector,
-            R.drawable.ease_chat_location_selector };
-    protected int[] itemIds = { ITEM_TAKE_PICTURE, ITEM_PICTURE, ITEM_LOCATION };
+//    protected int[] itemStrings = { R.string.attach_take_pic, R.string.attach_picture, R.string.attach_location };
+    protected int[] itemStrings = { R.string.attach_take_pic, R.string.attach_picture };
+    
+//    protected int[] itemdrawables = { R.drawable.ease_chat_takepic_selector, R.drawable.ease_chat_image_selector, R.drawable.ease_chat_location_selector };
+    protected int[] itemdrawables = { R.drawable.ease_chat_takepic_selector, R.drawable.ease_chat_image_selector };
+    
+//    protected int[] itemIds = { ITEM_TAKE_PICTURE, ITEM_PICTURE, ITEM_LOCATION };
+    protected int[] itemIds = { ITEM_TAKE_PICTURE, ITEM_PICTURE};
+    
     private EMChatRoomChangeListener chatRoomChangeListener;
     private boolean isMessageListInited;
     protected MyItemClickListener extendMenuItemClickListener;
